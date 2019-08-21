592b5cb2f297fd81d79870c7477de658462cab72
Bennett Dungan <bennettdungan@gmail.com>
2019-08-20 22:29:23 -0500

    added ThemeContext component for color changing demonstrations

--- src/app.js
+++ src/app.js
@@ -3,18 +3,22 @@ import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState("peru");
   return (
-    <div>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Router>
-        <SearchParams path="/" />
-        <Details path="/details/:id" />
-      </Router>
-    </div>
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
