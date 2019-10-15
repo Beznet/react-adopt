6387d715e596dd4d53061cfc6d3a8245a1c41c72
Bennett Dungan <bennettdungan@gmail.com>
2019-10-14 22:40:45 -0500

    using hooks with css, zoinks

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
 
-const NavBar = () => (
-  <header
-    css={css`
-      background-color: #333;
-      padding: 15px;
-    `}
-  >
-    <Link to="/">Adopt Me!</Link>
-    <span role="img" aria-label="logo">
-      🐶
-    </span>
-  </header>
-);
+const NavBar = () => {
+  const [pad, setPad] = useState(15);
+
+  return (
+    <header
+      onClick={() => setPad(pad + 15)}
+      css={css`
+        background-color: #333;
+        padding: ${pad}px;
+      `}
+    >
+      <Link to="/">Adopt Me!</Link>
+      <span role="img" aria-label="logo">
+        🐶
+      </span>
+    </header>
+  );
+};
 
 export default NavBar;
